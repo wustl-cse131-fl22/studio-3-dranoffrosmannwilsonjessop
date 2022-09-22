bf3150beca595454d09e1dbbf417002d750a1d06
charlesdranoff <charlesdranoff@charlessair954.lan>
2022-09-22 12:39:47 -0500

    Studio 3

--- src/studio3/Sieve.java
+++ src/studio3/Sieve.java
@@ -13,15 +13,16 @@ public class Sieve {
 		for (int i = 0; i < numberList.length - 1; i++){
 			numberList[i] = i + 2;
 		}
-		int u = 0;
-		for (int j = 2; j<Math.sqrt(N); j++) {
-			if (numberList[u] % j == 0 && numberList[u] != j){
-				numberList[u] = 0;
+		//int u = 2;
+		for (int u = 2; u < Math.sqrt(N); u++) {
+			for (int j = 0; j < numberList.length - 1; j++) {
+				if (numberList[j] % u == 0 && numberList[j] != u){
+					numberList[j] = 0;
+				}
+				//System.out.print(numberList[j] + " ");
+			}
 		}
-			u += 1;
-			System.out.print(numberList[u] + " ");
-		}
-			/*
+		/*
 			if (numberList[i] % 2 == 0 && numberList[i] != 2) {
 				numberList[i] = 0;
 			}
@@ -52,14 +53,14 @@ public class Sieve {
 			if (numberList[i] % 29 == 0 && numberList[i] != 29) {
 				numberList[i] = 0;
 			}
-			*/
-	
-	for (int k = 0; k < numberList.length -1; k++) {
+		 */
+
+		for (int k = 0; k < numberList.length -1; k++) {
 			if (numberList[k]!= 0) {
-				//System.out.print(numberList[k] + " ");
-				
+				System.out.print(numberList[k] + " ");
+
 			}
-			
+
 		}
 
 	}
